b43e8afa9bf26fce60aed1498447e29c917dd707
Julio Romero <julioale21@gmail.com>
2021-11-01 17:22:19 -0300

    added beforeEach statement

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -5,17 +5,20 @@ const newTodo = require('../mock-data/new-todo.json');
 
 TodoModel.create = jest.fn();
 
+let req, res, next;
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
+
 describe('TodoController.createTodo', () => {
   it('should have a createTodo function', () => {
     expect(typeof TodoController.createTodo).toBe('function');
   });
 
   it('should call TodoModel.create', () => {
-    let req, res, next;
-    req = httpMocks.createRequest();
-    res = httpMocks.createResponse();
     req.body = newTodo;
-    next = null;
     TodoController.createTodo(req, res, next);
     expect(TodoModel.create).toBeCalledWith(newTodo);
   });
