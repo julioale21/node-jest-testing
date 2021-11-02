a8f709611c8ef3d3c1ccfba050eceb3aa1df82ae
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-11-02 12:21:12 -0300

    added get todos integration test

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const todoController = require('../controllers/todo.controller');
 const router = express.Router();
 
+router.get('/', todoController.getTodos);
 router.post('/', todoController.createTodo);
 
 module.exports = router;
