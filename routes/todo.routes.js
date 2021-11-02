959a3f9f5b66fd9908555fb2b872f9b403845384
Julio Romero <julioale21@gmail.com>
2021-11-02 15:33:26 -0300

    added integration test to get todo by id

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -3,6 +3,7 @@ const todoController = require('../controllers/todo.controller');
 const router = express.Router();
 
 router.get('/', todoController.getTodos);
+router.get('/:todoId', todoController.getTodoById);
 router.post('/', todoController.createTodo);
 
 module.exports = router;
