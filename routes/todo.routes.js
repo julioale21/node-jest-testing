78ceeca27351268ba71630558fc97d6d01dd7035
Julio Romero <julioale21@gmail.com>
2021-11-02 22:46:48 -0300

    added integration test to update todo and route

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get('/', todoController.getTodos);
 router.get('/:todoId', todoController.getTodoById);
 router.post('/', todoController.createTodo);
+router.put('/:todoId', todoController.updateTodo);
 
 module.exports = router;
